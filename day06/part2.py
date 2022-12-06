9c7f65477c31337f085d805267e55c5b4b43c5cd
joshbarlow <barlow.joshua@gmail.com>
2022-12-06 14:28:42 +0000

    day 06

---
+++ day06/part2.py
@@ -0,0 +1,38 @@
+import re
+
+def importData():
+    with open('input.txt') as input_file:
+        inputDataArray = input_file.readlines()
+    cleanInputDataArray = []
+    for line in inputDataArray:
+        cleanInputDataArray.append(line.replace("\n", ""))
+    return cleanInputDataArray
+
+def calcStreamStart(inputDataArray):
+
+    stream = inputDataArray[0]
+
+    buffer = []
+    streamStart = 1
+
+    for char in stream:
+        # print(char)
+        if (len(buffer) == 14):
+            buffer.pop(0)
+            buffer.append(char)
+
+            bufferSet = set(buffer)
+
+            if (len(bufferSet) == len(buffer)):
+                break
+        
+        else:
+            buffer.append(char)
+
+        streamStart += 1
+        print(buffer)
+    
+    return streamStart
+
+if __name__ == '__main__':
+    print(calcStreamStart(importData()))
