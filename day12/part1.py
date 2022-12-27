051cb0cb3ecd28257d5ad8fdae7e6983ccdf3de2
joshbarlow <barlow.joshua@gmail.com>
2022-12-27 17:54:26 +0000

    wip

--- day12/part1.py
+++ day12/part1.py
@@ -14,8 +14,48 @@ def calculateShortestPath(inputDataArray):
         for x in range(len(inputDataArray[0])):
             nodeList.append(node(inputDataArray[y][x],x,y,len(inputDataArray[0]),len(inputDataArray)))
     
+    for x in nodeList:
+        if(x.id == 0):
+            x.pathCost = 0
+            x.active = True
     
+    count = 0
+    lock = True
+    FinalCost = 0
 
+    while lock:
+
+        lowestActiveNode = -1
+        lowestActiveRisk = 100000
+
+        for x in nodeList:
+            if(x.active == True):
+                if(x.pathCost < lowestActiveRisk):
+                    lowestActiveNode = x.id
+                    lowestActiveRisk = x.pathCost
+                # print(f"{x.id} - {x.neighbours}")
+        
+        for nNode in nodeList[lowestActiveNode].neighbours:
+            neighbourCurrentCost = nodeList[nNode].pathCost
+            neighbourNodeRisk = nodeList[nNode].risk
+            activePathCost = nodeList[lowestActiveNode].pathCost
+            if(neighbourCurrentCost > activePathCost + neighbourNodeRisk):
+                nodeList[nNode].pathCost = activePathCost + neighbourNodeRisk
+                nodeList[nNode].previousNode = lowestActiveNode
+                nodeList[nNode].active = True
+            
+            if(nodeList[nNode].id == (len(nodeList) - 1)):
+                # print('Found Path')
+                # print(f"Cost is {nodeList[nNode].pathCost}")
+                FinalCost = nodeList[nNode].pathCost
+                lock = False
+        
+        # print(f"LowestActive = {lowestActiveNode}")
+        
+        nodeList[lowestActiveNode].active = False
+
+        count += 1
+    
     return 0
 
 class node:
@@ -24,14 +64,17 @@ class node:
         
         self.x = x
         self.y = y
+        self.start = False
+        self.end = False
         self.height = ord(height) - 96
+        if(height == 'S'):
+            self.start = True
+        elif(height == 'E'):
+            self.end = True
 
         self.pathCost = 100000000
         self.previousNode = -1
         self.active = False
-
-        self.start = False
-        self.end = False
 
         self.id = idGen(x,y,lenX)
 
